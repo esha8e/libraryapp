21ca2ccb54bad651e43ed2178474146da76e6a89
esha8e <eshae8889@gmail.com>
2022-07-05 13:44:21 +0530

    component completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddbookComponent } from './addbook/addbook.component';
+import { SearchbookComponent } from './searchbook/searchbook.component';
+import { ViewbookComponent } from './viewbook/viewbook.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddbookComponent
+    AddbookComponent,
+    SearchbookComponent,
+    ViewbookComponent
   ],
   imports: [
     BrowserModule,
